6626d827694a3f3d29e1bb8fd32b98ab09796a03
Ayatullah Ma'arif <aya.mf12@gmail.com>
2024-05-06 21:11:19 +0700

    Initial Commit

---
+++ AuraSkin/AuraSkinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraSkinApp.swift
+//  AuraSkin
+//
+//  Created by Ayatullah Ma'arif on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AuraSkinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuraSkin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AuraSkin
+//
+//  Created by Ayatullah Ma'arif on 06/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
