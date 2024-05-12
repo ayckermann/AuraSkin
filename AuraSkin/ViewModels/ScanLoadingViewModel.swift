ad2111fa02f5a13fec7302b780deca48ab710000
Ayatullah Ma'arif <aya.mf12@gmail.com>
2024-05-12 23:19:22 +0700

    perf: #BLC007B reformat ocr output
    reformat ocr output to an array of string
    - breakdown output to an array with commas as separation
    - remove "ingredients:" from the value

--- AuraSkin/ViewModels/ScanLoadingViewModel.swift
+++ AuraSkin/ViewModels/ScanLoadingViewModel.swift
@@ -9,22 +9,37 @@ import SwiftUI
 
 struct ScanLoadingViewModel: View {
     var image: UIImage?
+    @State var resultWord: [String] = []
     
     var body: some View {
-        LoadingView()
-            .onAppear(perform: {
-                OpticalCharRecognition().performOCR(on: image!) { result in
-                    switch result {
-                    case .success(let recognizedText):
-                        print("Recognized Text:")
-                        print(recognizedText)
-                        // Handle recognized text here
-                    case .failure(let error):
-                        print("OCR Error: \(error.localizedDescription)")
-                        // Handle error here
+        
+        
+        if (!resultWord.isEmpty){
+            ScrollView{
+                VStack {
+                    ForEach(resultWord, id: \.self){ result in
+                        Text(result)
                     }
                 }
-            })
+            }
+            .padding(35)
+        }
+        
+        else{
+            LoadingView()
+                .onAppear(perform: {
+                    OpticalCharRecognition().performOCR(on: image!) { result in
+                        switch result {
+                        case .success(let words):
+                            resultWord = words
+                        case .failure(let error):
+                            print("OCR Error: \(error.localizedDescription)")
+                            // Handle error here
+                        }
+                    }
+                })
+        }
+        
     }
     
 }
@@ -32,3 +47,4 @@ struct ScanLoadingViewModel: View {
 //#Preview {
 //    ScanLoadingViewModel()
 //}
+
