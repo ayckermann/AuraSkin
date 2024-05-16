e9a8993c6a5682b2296e0d1983214c105f173f51
M. Ilham Syaputra <ilhamsyaputra25@gmail.com>
2024-05-17 00:10:01 +0700

    feat: add getIngredientAnalysis on APIServices

--- AuraSkin/Services/APIServices.swift
+++ AuraSkin/Services/APIServices.swift
@@ -7,61 +7,8 @@
 
 import Foundation
 
-extension AnalysisResultView {
-//    func apiCall() {
-//        guard let url = URL(string: "https://api.cosmily.com/api/v1/analyze/ingredient_list") else { return }
-//        
-//        // headers
-//        let headers = [
-//            "Content-Type": "application/json"
-//        ]
-//        
-//        // body
-//        let body = try? JSONSerialization.data(
-//            withJSONObject: ["ingredients": ingredients],
-//            options: []
-//        )
-//        
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "POST"
-//        request.httpBody = body
-//        
-//        for (key, value) in headers {
-//            request.setValue(value, forHTTPHeaderField: key)
-//        }
-//        
-//    //        send request
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//    //            handling error
-//            
-//            if error != nil {
-//                print(error?.localizedDescription)
-//                return
-//            }
-//            
-//            guard let data = data else { return }
-//    //        print(String(data: data, encoding: .utf8))
-//            
-//            
-//    //            decode the json data
-//            do {
-//                let apiData = try JSONDecoder().decode(IngredientsAnalysisResponse.self, from: data)
-//                
-//                self.data = apiData
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//            
-//    //        return apiResponse
-//            
-//            
-//        }
-//        .resume()
-//    }
-}
-
 class APIServices {
-    func getIngredientAnalysis() async throws -> IngredientsAnalysisResponse {
+    func getIngredientAnalysis(_ ingredients: String) async throws -> IngredientsAnalysisResponse {
         var apiResponse: IngredientsAnalysisResponse = IngredientsAnalysisResponse()
         guard let url = URL(string: "https://api.cosmily.com/api/v1/analyze/ingredient_list") else { return apiResponse }
 
@@ -72,7 +19,7 @@ class APIServices {
 
         // body
         let body = try? JSONSerialization.data(
-            withJSONObject: ["ingredients": ingredientsDummy],
+            withJSONObject: ["ingredients": ingredients],
             options: []
         )
 
@@ -85,25 +32,26 @@ class APIServices {
         }
 
         // send request
-        URLSession.shared.dataTask(with: request) { data, response, error in
-        // handling error
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
 
-            if error != nil {
-                print(error?.localizedDescription)
-                return
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 201 else {
+                print("Invalid response or status code")
+                return apiResponse
             }
 
-            guard let data = data else { return }
-            // print(String(data: data, encoding: .utf8))
+            print(String(data: data, encoding: .utf8) ?? "No response data")
 
-
-        // decode the json data
+            // decode the json data
             do {
                 apiResponse = try JSONDecoder().decode(IngredientsAnalysisResponse.self, from: data)
             } catch {
                 print(error.localizedDescription)
             }
+        } catch {
+            print("Request failed with error: \(error.localizedDescription)")
         }
+
         return apiResponse
     }
 }
