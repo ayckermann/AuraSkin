1f5ea32a9ca38027a4b1adc58127569e7adf7b2c
Ayatullah Ma'arif <aya.mf12@gmail.com>
2024-05-20 14:41:38 +0700

    Squashed commit of the following:
    commit 007c014aaa579ea31682d7de40b89b781e525bb2
    Merge: 8eb454b fd4e338
    Author: Ayatullah Ma'arif <88651324+ayckermann@users.noreply.github.com>
    Date:   Sat May 18 12:14:40 2024 +0700
    
        Merge pull request #7 from ayckermann/ScanScheme
    
        Merge branch Scan scheme

---
+++ AuraSkin/Views/ScanInstructionView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanInstructionView.swift
+//  AuraSkin
+//
+//  Created by Ayatullah Ma'arif on 20/05/24.
+//
+
+import SwiftUI
+
+struct ScanInstructionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanInstructionView()
+}
