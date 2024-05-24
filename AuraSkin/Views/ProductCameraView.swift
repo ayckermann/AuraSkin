6dcfc25f17b4f3030d9296f81002ae3f3934689a
anggrainiA <anggrainisri225@gmail.com>
2024-05-24 19:11:04 +0700

    feat: create camera view for product

--- AuraSkin/Views/ProductCameraView.swift
+++ AuraSkin/Views/ProductCameraView.swift
@@ -16,45 +16,81 @@ struct ProductCameraView: View {
     @State var capturedImage: UIImage?
     @State var isCaptured = false
     
+    
     var body: some View {
         ZStack{
             Color(.black)
-                .ignoresSafeArea(.all)
             
-            CameraView(ratioX: 9.0, ratioY: 16.0, cameraServices: cameraService){ result in
-                switch result {
+            if(capturedImage != nil){
+                VStack{
+                    tempShowPhotoView(image: capturedImage!)
                     
-                case .success(let photo):
-                    if let data = photo.fileDataRepresentation(){
-                        capturedImage = UIImage(data: data)
-                        isCaptured = true
+                    Button(action: {
+                        capturedImage = nil
+                    }, label: {
+                        TestButton(text: "reset")
                         
-                    }
-                    else{
-                        print("Error: no image data found")
-                    }
-                case .failure(let error):
-                    print(error.localizedDescription)
-                    
+                    })
                 }
             }
-            
-            VStack{
-                Spacer()
-                Button(action: {
-                    cameraService.capturePhoto()
-                }, label: {
-                    Image(systemName: "circle")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 75)
-                        .foregroundStyle(.white)
-                })
-                Spacer()
-                    .frame(height: 28)
+            else{
+                CameraView(ratioX: 1.0, ratioY: 1.0, cameraServices: cameraService){ result in
+                    switch result {
+                        
+                    case .success(let photo):
+                        if let data = photo.fileDataRepresentation(){
+                            capturedImage = UIImage(data: data)
+//                            print(capturedImage!.size.width)
+//                            capturedImage = cropImage(capturedImage!,  aspectRatio: 4/3, width: capturedImage!.size.width)
+//                            capturedImage = crop(image: capturedImage!, to: 4/3)
+
+                            isCaptured = true
+                            
+                        }
+                        else{
+                            print("Error: no image data found")
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                        
+                    }
+                }
+                .padding(EdgeInsets( top: 0, leading: 0, bottom: 400, trailing: 0))
+                
+                VStack{
+                    Spacer()
+                    Button(action: {
+                        cameraService.capturePhoto()
+
+                        
+                    }, label: {
+                        Image(systemName: "circle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 75)
+                            .foregroundStyle(.white)
+                    })
+                    Spacer()
+                        .frame(height: 28)
+                }
+                
             }
-            
         }
+//        .toolbar(.hidden, for: .tabBar)
+    
+        
+        
+        
+        
+    }
+}
+
+struct tempShowPhotoView : View {
+    var image: UIImage
+    var body: some View {
+        Image(uiImage: image)
+            .resizable()
+            .scaledToFit()
     }
 }
 
