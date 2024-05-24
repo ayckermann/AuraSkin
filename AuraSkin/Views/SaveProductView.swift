2d8dd5196c288b0e3ff68944cded7c4a0c3d8ab7
Ayatullah Ma'arif <aya.mf12@gmail.com>
2024-05-24 13:47:41 +0700

    Squashed commit of the following:
    commit ce6681a1efafca1576d6c03084948cacf53d8e68
    Merge: cbb272c 32ec755
    Author: M. Ilham Syaputra <56667273+ilhamsyaputra@users.noreply.github.com>
    Date:   Fri May 24 13:17:03 2024 +0700
    
        Merge pull request #20 from ayckermann/saved-product
    
        BLC-006A: Saved Product Function Work - Require Update
    
    commit 32ec7559087123b4cce0251227e0e25724c623da
    Merge: 6df8e08 cbb272c
    Author: M. Ilham Syaputra <56667273+ilhamsyaputra@users.noreply.github.com>
    Date:   Fri May 24 13:16:54 2024 +0700
    
        Merge branch 'main' into saved-product
    
    commit 6df8e08ad02b45ef0b50db70cb53104153a39b0b
    Author: Syuhada Rantisi <suadarantisi@gmail.com>
    Date:   Fri May 24 02:39:25 2024 +0700
    
        BLC-006A: Saved Product Function Work - Require Update
    
    commit cbb272ca8b0e4acf66ca3b8f8b56e058b5a70333
    Merge: 448aa78 af87446
    Author: Ayatullah Ma'arif <88651324+ayckermann@users.noreply.github.com>
    Date:   Fri May 24 01:46:38 2024 +0700
    
        Merge pull request #19 from ayckermann/ScanScheme
    
        Scan scheme

--- AuraSkin/Views/SaveProductView.swift
+++ AuraSkin/Views/SaveProductView.swift
@@ -6,14 +6,19 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct SaveProductView: View {
-    @State var productName: String = ""
-    @State var skincareCategory: String = "Facial Wash"
-    @State var expiredDate: Date = Date.now
-    @State var currentlyUsed: Bool = true
-
+    // MARK: Core data
+    @EnvironmentObject var manager: CoreDataManager
+    @Environment(\.managedObjectContext) var viewContext
+    
     @Environment(\.dismiss) var dismiss
+    
+    @State var product: ProductModel = ProductModel(name: "", ingredients: "ingredients", category: "Facial Wash", currentlyUsed: true, expiredDate: Date.now, image: UIImage(named: "productImageDefault")?.pngData())
+    
+    @State private var photosPickerItem: PhotosPickerItem?
+    var saveProductViewModel = SaveProductViewModel()
 
     var basicSkincare: [String] = ["Facial Wash", "Toner", "Moisturizer", "Sunscreen"]
 
@@ -24,6 +29,12 @@ struct SaveProductView: View {
                     .resizable()
                     .frame(width: 55, height: 55)
                     .foregroundColor(.gray)
+                PhotosPicker(selection: $photosPickerItem, matching: .images){
+                    Image(uiImage: UIImage(data: product.image ?? UIImage(named: "productImageDefault")!.pngData()!) ?? UIImage(named: "productImageDefault")!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 100, height: 100)
+                }
             }
             .frame(width: 250, height: 235)
             .overlay(RoundedRectangle(cornerRadius: 11).stroke(.gray, lineWidth: 1))
@@ -31,31 +42,46 @@ struct SaveProductView: View {
             .onTapGesture {
                 // launch camera view
             }
+            .onChange(of: photosPickerItem) { _, _ in
+                Task {
+                    if let photosPickerItem,
+                       let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
+                        if let image = UIImage(data: data) {
+                            product.image = data
+                        }
+                    }
+                    photosPickerItem = nil
+                }
+            }
 
             SectionTextLeading("Product's Name")
                 .foregroundStyle(Color(UIColor.darkGray))
-            TextFieldComponents(text: $productName, localizedStringKey: "product's name",  prompt: "product's name")
+
+            TextFieldComponents(text: $product.name, localizedStringKey: "product's name", prompt: "product's name")
                 .padding(.bottom)
 
             SectionTextLeading("Basic Skincare Category")
                 .foregroundStyle(Color(UIColor.darkGray))
-            DropdownComponent(selectedOption: $skincareCategory, title: "Category", items: basicSkincare)
+            DropdownComponent(selectedOption: $product.category, title: "Category", items: basicSkincare)
                 .padding(.bottom)
 
             SectionTextLeading("Expired Date")
                 .foregroundStyle(Color(UIColor.darkGray))
-            DatePickerComponent(expiredDate: $expiredDate)
+            DatePickerComponent(expiredDate: $product.expiredDate)
                 .padding(.bottom)
 
-
-            Toggle("Currently used", isOn: $currentlyUsed)
+            Toggle("Currently used", isOn: $product.currentlyUsed)
                 .frame(height: 45)
                 .tint(Color.auraSkinPrimaryColor)
                 .padding([.leading, .trailing], 15)
                 .overlay(RoundedRectangle(cornerRadius: 5).stroke(.gray, lineWidth: 1))
                 .padding(.bottom)
 
-            TestButton(text: "Save")
+            Button(action: {
+                saveProductViewModel.saveProduct(context: viewContext, product: product)
+            }, label: {
+                TestButton(text: "Save")
+            })
         }
         .navigationTitle("Add Product")
         .navigationBarTitleDisplayMode(.inline)
@@ -79,3 +105,4 @@ struct SaveProductView: View {
 #Preview {
     SaveProductView()
 }
+
