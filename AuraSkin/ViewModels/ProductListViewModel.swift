a5352b8f8cbc145a1a0418dd682a5165db900c36
Risma A. Harby <76735341+rismaananda@users.noreply.github.com>
2024-05-24 18:27:01 +0700

    change dummy product

--- AuraSkin/ViewModels/ProductListViewModel.swift
+++ AuraSkin/ViewModels/ProductListViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 class ProductListViewModel: ObservableObject {
-    @Published var products = [Product]()
+    @Published var products = [ProductDummy]()
     @Published var searchText: String = ""
     @Published var selectedCategory: String = "Facial Wash"
     
@@ -17,38 +17,38 @@ class ProductListViewModel: ObservableObject {
 
     
     let dummyData = [
-        Product(name: "Gaung",category: "Facial Wash", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
-        Product(name: "Alif",category: "Toner", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
-        Product(name: "Harby",category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
-        Product(name: "Sample Product 4", category: "Sunscreen", description: "Description for sample product 4", imageUrl: "chetapil"),
-        Product(name: "Sample Product 5", category: "Moisturizer", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
-        Product(name: "Sample Product 6", category: "Sunscreen", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
-        Product(name: "Sample Product 7", category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
-        Product(name: "Sample Product 8", category: "Toner", description: "Description for sample product 4", imageUrl: "chetapil"),
-        Product(name: "Gaung",category: "Facial Wash", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
-        Product(name: "Alif",category: "Toner", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
-        Product(name: "Harby",category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
-        Product(name: "Sample Product 4", category: "Sunscreen", description: "Description for sample product 4", imageUrl: "chetapil"),
-        Product(name: "Sample Product 5", category: "Moisturizer", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
-        Product(name: "Sample Product 6", category: "Sunscreen", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
-        Product(name: "Sample Product 7", category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
-        Product(name: "Sample Product 8", category: "Toner", description: "Description for sample product 4", imageUrl: "chetapil"),
-        Product(name: "Gaung",category: "Facial Wash", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
-        Product(name: "Alif",category: "Toner", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
-        Product(name: "Harby",category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
-        Product(name: "Sample Product 4", category: "Sunscreen", description: "Description for sample product 4", imageUrl: "chetapil"),
-        Product(name: "Sample Product 5", category: "Moisturizer", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
-        Product(name: "Sample Product 6", category: "Sunscreen", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
-        Product(name: "Sample Product 7", category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
-        Product(name: "Sample Product 8", category: "Toner", description: "Description for sample product 4", imageUrl: "chetapil"),
-        Product(name: "Gaung",category: "Facial Wash", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
-        Product(name: "Alif",category: "Toner", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
-        Product(name: "Harby",category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
-        Product(name: "Sample Product 4", category: "Sunscreen", description: "Description for sample product 4", imageUrl: "chetapil"),
-        Product(name: "Sample Product 5", category: "Moisturizer", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
-        Product(name: "Sample Product 6", category: "Sunscreen", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
-        Product(name: "Sample Product 7", category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
-        Product(name: "Sample Product 8", category: "Toner", description: "Description for sample product 4", imageUrl: "chetapil")
+        ProductDummy(name: "Gaung",category: "Facial Wash", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
+        ProductDummy(name: "Alif",category: "Toner", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
+        ProductDummy(name: "Harby",category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
+        ProductDummy(name: "Sample Product 4", category: "Sunscreen", description: "Description for sample product 4", imageUrl: "chetapil"),
+        ProductDummy(name: "Sample Product 5", category: "Moisturizer", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
+        ProductDummy(name: "Sample Product 6", category: "Sunscreen", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
+        ProductDummy(name: "Sample Product 7", category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
+        ProductDummy(name: "Sample Product 8", category: "Toner", description: "Description for sample product 4", imageUrl: "chetapil"),
+        ProductDummy(name: "Gaung",category: "Facial Wash", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
+        ProductDummy(name: "Alif",category: "Toner", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
+        ProductDummy(name: "Harby",category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
+        ProductDummy(name: "Sample Product 4", category: "Sunscreen", description: "Description for sample product 4", imageUrl: "chetapil"),
+        ProductDummy(name: "Sample Product 5", category: "Moisturizer", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
+        ProductDummy(name: "Sample Product 6", category: "Sunscreen", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
+        ProductDummy(name: "Sample Product 7", category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
+        ProductDummy(name: "Sample Product 8", category: "Toner", description: "Description for sample product 4", imageUrl: "chetapil"),
+        ProductDummy(name: "Gaung",category: "Facial Wash", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
+        ProductDummy(name: "Alif",category: "Toner", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
+        ProductDummy(name: "Harby",category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
+        ProductDummy(name: "Sample Product 4", category: "Sunscreen", description: "Description for sample product 4", imageUrl: "chetapil"),
+        ProductDummy(name: "Sample Product 5", category: "Moisturizer", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
+        ProductDummy(name: "Sample Product 6", category: "Sunscreen", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
+        ProductDummy(name: "Sample Product 7", category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
+        ProductDummy(name: "Sample Product 8", category: "Toner", description: "Description for sample product 4", imageUrl: "chetapil"),
+        ProductDummy(name: "Gaung",category: "Facial Wash", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
+        ProductDummy(name: "Alif",category: "Toner", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
+        ProductDummy(name: "Harby",category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
+        ProductDummy(name: "Sample Product 4", category: "Sunscreen", description: "Description for sample product 4", imageUrl: "chetapil"),
+        ProductDummy(name: "Sample Product 5", category: "Moisturizer", description: "Description for sample product 1", imageUrl: "unsplash_p3O5f4u95Lo"),
+        ProductDummy(name: "Sample Product 6", category: "Sunscreen", description: "Description for sample product 2", imageUrl: "unsplash_ZIihTgf5uGg"),
+        ProductDummy(name: "Sample Product 7", category: "Moisturizer", description: "Description for sample product 3", imageUrl: "avoskin"),
+        ProductDummy(name: "Sample Product 8", category: "Toner", description: "Description for sample product 4", imageUrl: "chetapil")
 
     ]
     
@@ -56,14 +56,14 @@ class ProductListViewModel: ObservableObject {
         products = dummyData
     }
     
-    var filteredProducts: [Product] {
+    var filteredProducts: [ProductDummy] {
         guard !searchText.isEmpty else { return products }
         return products.filter { product in
             product.name.lowercased().contains(searchText.lowercased())
         }
     }
     
-    var filteredSegmentProducts: [Product] {
+    var filteredSegmentProducts: [ProductDummy] {
            products.filter { product in
                (selectedCategory.isEmpty || product.category == selectedCategory) &&
                (searchText.isEmpty || product.name.lowercased().contains(searchText.lowercased()))
