86e019881b75ef390c4fb693c44e0820ddd23ec7
Syuhada Rantisi <suadarantisi@gmail.com>
2024-05-26 17:49:56 +0700

    BLC-009D: Edit Product, Delete Product, Back to image binary

---
+++ AuraSkin/Views/EditProductView.swift
@@ -0,0 +1,137 @@
+//
+//  SaveProductView.swift
+//  AuraSkin
+//
+//  Created by Syuhada Rantisi on 26/05/24.
+//
+
+import SwiftUI
+import PhotosUI
+import CoreData
+
+struct EditProductView: View {
+    // MARK: Core data
+//    @EnvironmentObject var manager: CoreDataManager
+//    @Environment(\.managedObjectContext) var viewContext
+    enum photoStateEnum {
+        case noImage, image
+    }
+    
+    @State private var photosPickerItem: PhotosPickerItem?
+    @State private var photoState: photoStateEnum = .noImage
+    
+    @Binding var context: NSManagedObjectContext
+    @Binding var product: Product
+    @Binding var isPresentedEdit: Bool
+    
+    var saveProductViewModel = SaveProductViewModel()
+    
+    var basicSkincare: [String] = ["Facial Wash", "Toner", "Moisturizer", "Sunscreen"]
+    @State var category: String = "Facial Wash"
+    @State var expiredDate: Date = Date()
+    
+    
+    var body: some View {
+        VStack {
+            PhotosPicker(selection: $photosPickerItem, matching: .images) {
+                switch photoState {
+                case .noImage:
+                    Image(systemName: "plus")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 55, height: 55)
+                        .foregroundStyle(.gray)
+                        .frame(width: 250, height: 235)
+                        .overlay(RoundedRectangle(cornerRadius: 11).stroke(.gray))
+                        .padding(.bottom)
+                case .image:
+                    Image(data: (product.image ?? UIImage(named: "productImageDefault")?.pngData())!)!
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 250, height: 235)
+                        .foregroundColor(.gray)
+                        .clipShape(RoundedRectangle(cornerRadius: 11))
+                        .padding(.bottom)
+                }
+            }
+            .onChange(of: photosPickerItem) { _, _ in
+                Task {
+                    if let photosPickerItem,
+                       let data = try? await photosPickerItem.loadTransferable(type: Data.self) {
+                        if UIImage(data: data) != nil {
+                            product.image = data
+                            self.photoState = .image
+                        }
+                    }
+                    photosPickerItem = nil
+                }
+            }
+            
+            
+            SectionTextLeading("Product's Name")
+                .foregroundStyle(Color(UIColor.darkGray))
+            
+            TextFieldComponents(text: $product.name.unwrapped, localizedStringKey: "product's name", prompt: "product's name")
+                .padding(.bottom)
+            
+            SectionTextLeading("Basic Skincare Category")
+                .foregroundStyle(Color(UIColor.darkGray))
+            DropdownComponent(selectedOption: $category, title: "Category", items: basicSkincare)
+                .padding(.bottom)
+                .onChange(of: category){
+                    Task {
+                        product.category = category
+                    }
+                }
+            
+            SectionTextLeading("Expired Date")
+                .foregroundStyle(Color(UIColor.darkGray))
+            DatePickerComponent(expiredDate: $expiredDate)
+                .padding(.bottom)
+                .onChange(of: expiredDate){
+                    product.expiredDate = expiredDate
+                }
+            
+            ToggleComponent(text: "Currently used", isOn: $product.currentlyUsed)
+            
+            Button(action: {
+                saveProductViewModel.updateProduct(context: context, product: product)
+                isPresentedEdit = false
+
+            }, label: {
+                TestButton(text: "Save")
+            })
+        }
+        .navigationTitle("Add Product")
+        .navigationBarTitleDisplayMode(.inline)
+        .padding()
+        .padding(.top, 10)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    isPresentedEdit = false
+                } label: {
+                    Image(systemName: "chevron.left")
+                    Text("Cancel")
+                }
+                .foregroundStyle(Color.auraSkinPrimaryColor)
+            }
+        }
+        .toolbarBackground(.visible, for: .navigationBar)
+        .onAppear {
+            category = product.category ?? ""
+            expiredDate = product.expiredDate ?? Date()
+            
+            if(product.image != nil){
+                photoState = photoStateEnum.image
+            }
+        }
+    }
+    
+    
+}
+
+//#Preview {
+//    EditProductView(ingredients: "Sample ingredients")
+//}
+
